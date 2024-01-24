8c4024e3d1400be583b1aaf00e97e83fc5542617
Utkarsh Gupta <utkarshgupta04092003@gmail.com>
2024-01-24 15:59:14 +0530

    bug fixed

--- src/Components/App.js
+++ src/Components/App.js
@@ -6,31 +6,34 @@ import ContactList from './ContactList';
 import Header from './Header';
 //use state is a hook used to manipulate state when some data is being fetched from a server
 function App() {
-  const LOCAL_STORAGE_KEY="Contacts"
-  const [Contacts,setContacts]=useState([])//variable on component whose state is changed -"Contacts"
-const addContactHandler=(contact)=>{
+  const LOCAL_STORAGE_KEY = "Contacts";
+  const [Contacts, setContacts] = useState([])//variable on component whose state is changed -"Contacts"
+  const addContactHandler = (contact) => {
     console.log(contact)
-    setContacts([...Contacts,{id:uuid(),...Contacts}]);
-  }
-  useEffect(()=>{
-    const retrieveContact=JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
-    if(retrieveContact) {setContacts(retrieveContact)}
-  },[])
-    //add the dependency to keep the content on local storage
+    setContacts([...Contacts, contact ]);
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify([...Contacts, contact ]))//convrt into string
 
-  useEffect(()=>{
-    localStorage.setItem(LOCAL_STORAGE_KEY,JSON.stringify(Contacts))//convrt into string
-  },
-    //add the dependency to keep the content on local storage
-    [Contacts] 
-  );
+  }
+  useEffect(() => {
+    const retrieveContact = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    console.log('retrieve', retrieveContact);
+    if (retrieveContact) { setContacts(retrieveContact) }
+  }, [])
+  //add the dependency to keep the content on local storage
+
+  // useEffect(() => {
+  //   localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(Contacts))//convrt into string
+  // },
+  //   //add the dependency to keep the content on local storage
+  //   [Contacts]
+  // );
   return (
     <div className='ui container'>
-      <Header/>
-      <AddContact addContactHandler={addContactHandler}/>{/*Here the content from child is called into parent*/}
-      <ContactList contacts={Contacts}/>{/*here the state is being changed and transfered from parent to child*/}
+      <Header />
+      <AddContact addContactHandler={addContactHandler} />{/*Here the content from child is called into parent*/}
+      <ContactList contacts={Contacts} />{/*here the state is being changed and transfered from parent to child*/}
       {/*The props i.e, property of ContactList*/}
-      
+
     </div>
   );
 }
--- src/Components/ContactList.js
+++ src/Components/ContactList.js
@@ -4,9 +4,9 @@ import ContactCard from './ContactCard';
 const ContactList=(props)=>{
     
     //To render it to the App Component inside the ContactList component
-    const renderContactList=props.contacts.map((contact)=> {//contact attribute is a list of every single record of array in Contacts 
+    const renderContactList=props.contacts.map((contact,index)=> {//contact attribute is a list of every single record of array in Contacts 
         return(
-           <ContactCard contact={contact} />
+           <ContactCard contact={contact} key={index}/>
         )
         
     }) 
